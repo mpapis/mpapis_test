c7c41d1a910834653ef3f51cf3e1b6586fa2ae15
mpapis <mpapis@gmail.com>
2011-08-04 14:55:25 +0200

    updated bdsm shebang example

--- test_bdsm_shebang.sh
+++ test_bdsm_shebang.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bdsm
 
-if module 'backup'
-then
-  log "Check https://github.com/mpapis/bdsm_backup for instructions"
-else
-  error "backup extension not available, install it with: "\
-    "bdsm extend mpapis/bdsm_backup backup"
-fi
+module_or_error 'backup/scripting'\
+  "backup extension not available, install it with: bdsm extend mpapis/bdsm_backup backup"
+
+log "Check https://github.com/mpapis/bdsm_backup for instructions"
