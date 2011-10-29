97b6a497c063f14a35c2ce719e0cf7e22a1e99ce
mpapis <mpapis@gmail.com>
2011-10-30 00:07:09 +0200

    refactoring the extended popen example

--- interactive/io_popen_ext.rb
+++ interactive/io_popen_ext.rb
@@ -10,37 +10,43 @@ end
 
 def process(output)
   th=Thread.current
+  curr="cmd_#{th['current']}".to_sym
+
   if /=====cmd:start=(.*)/ =~ output
-    th['current']||=0
-    th['current']+=1
-    curr="cmd_#{th['current']}".to_sym
     th[curr]={:command => $1}
+
   elsif /^=====cmd:stop=(.*)=(.*)$/ =~ output
-    curr="cmd_#{th['current']}".to_sym
     th[curr][:status]=$1.to_i
-    y th[curr]
+    th['current']+=1
+
   else
-    curr="cmd_#{th['current']}".to_sym
     th[curr][:lines]||=[]
     th[curr][:lines]+=[output]
   end
+
+  th[curr]
 end
 
 puts "Starting interactive session"
 IO.popen('bash', 'w+') { |bash|
+
   treader=Thread.new do
+    Thread.current['current']=0
+
     while !bash.eof?
-      process bash.gets
+      result = process bash.gets
+
+      unless result[:status].nil?
+        y result
+      end
     end
   end
+
   run bash, 'rvm info 1'
   run bash, 'rvm info 2'
   run bash, 'rvm info 3'
+
   run bash, 'exit'
   treader.join
-#  [0...treader['current']].each do |n|
-#    curr="cmd_#{n}".to_sym
-#    y treader[curr]
-#  end
 }
 puts "Ending interactive session"
