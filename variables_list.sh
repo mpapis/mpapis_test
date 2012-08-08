86441415fb4bddabea5b8b3d118503d71fcb8fd1
Michal Papis <mpapis@gmail.com>
2012-08-08 07:44:10 +0200

    fix compabiliti with older stuff

--- variables_list.sh
+++ variables_list.sh
@@ -8,23 +8,26 @@
 ## BASH does not interpret new lines in strings, ZSH does
 ##
 ## they both escape strings in '' if contains spaces, ' is escaped as '\''
+## strings might be prefixed with $
 
 variable1=play\'me
-variable2="play with
- me"
+variable2='play
+ with
+ me
+ now'
 array2=( four five six )
 array2[10]="ten"
 
-
-for v in $( set | grep -Eao "^[^=]*=[\('\"]?" )
+for v in $( set | grep -Eao "^[^=]*=[\('\$]?" )
 do
   x="${v##*=}"
   if [[ -n "$x" ]]
   then
-    y="${x/\(/\)}"
-    v="${v%\(}"
+    y="${x/\(/)}"
+    y="${y/\$/\'}"
+    v="${v%$x}"
     v="${v//\*/\*}"
-    set | awk 'BEGIN {m=0} /'^"${v}"'/{m=1} m {print} /'"$y$"'/{m=0}'
+    set | sed -n '/^'"$v"'/{: rep ; /'"$y$"'/!{N; b rep; }; p;}'
   else
     set | grep -a "^$v"
   fi
