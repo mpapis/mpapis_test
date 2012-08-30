1b1d63c81404b9a71bff64ae36fefba2a89e5192
Michal Papis <mpapis@gmail.com>
2012-08-30 05:16:51 +0200

    awk calculate downloads count for github repository

---
+++ count_downloads.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+curl -L https://api.github.com/repos/${1:-railsinstaller/railsinstaller-nix}/downloads |
+awk -F": |\"|," '
+  BEGIN {
+    sum=0;
+  }
+  $1=="    " && $2=="html_url" {
+    gsub(/.*\//,"",$5);
+    url=$5;
+  }
+  $1=="    " && $2=="download_count" {
+    count=$4;
+  }
+  $1=="  }" {
+    print url" "count;
+    sum+=count;
+  }
+  END {
+    print sum;
+  }
+'
